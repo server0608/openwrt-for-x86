0d9d767943ef82de055480a1fa1840205b51a97f
server0608 <63848847+server0608@users.noreply.github.com>
2023-09-16 17:56:58 +0800

    修复源错误

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/vernesong/OpenClash package/openclash
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packagessrc-git small https://github.com/kenzok8/small
-src-git haibo https://github.com/haiibo/openwrt-packages
-src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" Makefile
